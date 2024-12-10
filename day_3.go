e132fe9dca3225736f41a1c8e77f8d4089a7db80
Zitzeronion <physiknerd@gmail.com>
2024-12-10 07:45:11 +0100

    More work

--- day_3.go
+++ day_3.go
@@ -18,39 +18,64 @@ func main() {
 	re := regexp.MustCompile(`[mul(]+\d[\d,]*[\d{2}]?[)]`)
 	re2 := regexp.MustCompile("[0-9]+")
 	res := 0
-	star := 2
+	star := 1
 	operation := "mul"
+	linenumber := 0
+	nextline := "whazup"
 	// Loop over all lines in the file and print them.
 	for scanner.Scan() {
+		fmt.Println("We are reading line: ", linenumber)
 		line := scanner.Text()
 		s := re.FindAllStringSubmatch(line, -1)
 		// fmt.Println(s)
 		if star == 1 {
-			for i := 0; i < len(s); i++ {
-				if strings.Contains(s[i][0], operation) {
-					parts := strings.Split(s[i][0], ",")
-					if len(parts) == 2 {
-						firstNum := re2.FindString(parts[0])
-						secNum := re2.FindString(parts[1])
-						num0, _ := strconv.Atoi(firstNum)
-						num1, _ := strconv.Atoi(secNum)
-						mul := num0 * num1
-						res += mul
-					} else {
-						continue
-					}
-				}
-			}
+			checkString(res, s, operation)
 		} else if star == 2 {
-			fmt.Println(line)
 			star2 := strings.Split(line, "do")
 			for i := 0; i < len(star2); i++ {
-				if star2[i][0:2] == "()" || i == 0 {
+				star2[i] = nextline + star2[i]
+				if linenumber == 0 && star2[i][0:3] != "n't" {
+					fmt.Println("First line")
+					fmt.Println(star2[i])
+					nextline = "()"
+					for i := 0; i < len(s); i++ {
+						if strings.Contains(s[i][0], operation) {
+							parts := strings.Split(s[i][0], ",")
+							if len(parts) == 2 {
+								firstNum := re2.FindString(parts[0])
+								secNum := re2.FindString(parts[1])
+								num0, _ := strconv.Atoi(firstNum)
+								num1, _ := strconv.Atoi(secNum)
+								mul := num0 * num1
+								res += mul
+							} else {
+								continue
+							}
+						}
+					}
+				} else if star2[i][0:2] == "()" {
 					fmt.Println("Multiply me")
 					fmt.Println(star2[i])
+					nextline = "()"
+					for i := 0; i < len(s); i++ {
+						if strings.Contains(s[i][0], operation) {
+							parts := strings.Split(s[i][0], ",")
+							if len(parts) == 2 {
+								firstNum := re2.FindString(parts[0])
+								secNum := re2.FindString(parts[1])
+								num0, _ := strconv.Atoi(firstNum)
+								num1, _ := strconv.Atoi(secNum)
+								mul := num0 * num1
+								res += mul
+							} else {
+								continue
+							}
+						}
+					}
 				} else if star2[i][0:3] == "n't" {
 					fmt.Println("Throw me away")
 					fmt.Println(star2[i])
+					nextline = "n't"
 				} else {
 					fmt.Println("Think about")
 					fmt.Println(star2[i])
@@ -58,10 +83,31 @@ func main() {
 				//fmt.Println(star2[i][0:3])
 			}
 		}
+		linenumber++
 	}
 	if star == 1 {
 		fmt.Println("Result first star: ", res)
 	} else if star == 2 {
 		fmt.Println("Result second star: ", res)
 	}
+}
+
+func checkString(res int, s [][]string, operation string) {
+	re2 := regexp.MustCompile("[0-9]+")
+	for i := 0; i < len(s); i++ {
+		if strings.Contains(s[i][0], operation) {
+			parts := strings.Split(s[i][0], ",")
+			if len(parts) == 2 {
+				firstNum := re2.FindString(parts[0])
+				secNum := re2.FindString(parts[1])
+				num0, _ := strconv.Atoi(firstNum)
+				num1, _ := strconv.Atoi(secNum)
+				mul := num0 * num1
+				res += mul
+			} else {
+				continue
+			}
+		}
+	}
+	fmt.Println("Result : ", res)
 }
