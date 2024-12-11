6f390c1cd279d9bf0087e2320bac1aae5bcbbc94
Stefan Zitz <szitz@rcpepc01174.rcpe.pharm.local>
2024-12-11 12:33:28 +0100

    Left to right and right to left

--- day_4.go
+++ day_4.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	// Open the file.
 	f, _ := os.Open("exampleinput_4.txt")
-	xmascounter := 0
+	xmasLRcounter := 0
+	xmasRLcounter := 0
 	// data, _ := os.ReadFile("exampleinput_4.txt")
 	// Create a new Scanner for the file.
 	scanner := bufio.NewScanner(f)
@@ -17,17 +18,35 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		// fmt.Println(string(line[0]))
-		xmascounter += readL2R(line)
+		xmasLRcounter += readL2R(line)
+		xmasRLcounter += readR2L(line)
 	}
-	fmt.Println("Left to right XMAS: ", xmascounter)
+	fmt.Println("Left to right XMAS: ", xmasLRcounter)
+	fmt.Println("Right to left XMAS: ", xmasRLcounter)
 }
 func readL2R(text string) int {
 	xmascount := 0
 	for i := 0; i < len(text); i++ {
 		if string(text[i]) == "X" && i+3 < len(text) {
-			if string(text[i+1]) == "M" && i+1 < len(text) {
-				if string(text[i+2]) == "A" && i+2 < len(text) {
-					if string(text[i+3]) == "S" && i+3 < len(text) {
+			if string(text[i+1]) == "M" {
+				if string(text[i+2]) == "A" {
+					if string(text[i+3]) == "S" {
+						xmascount += 1
+					}
+				}
+			}
+		}
+	}
+	return xmascount
+}
+
+func readR2L(text string) int {
+	xmascount := 0
+	for i := len(text) - 1; i > -1; i-- {
+		if string(text[i]) == "X" && i-3 > 0 {
+			if string(text[i-1]) == "M" {
+				if string(text[i-2]) == "A" {
+					if string(text[i-3]) == "S" {
 						xmascount += 1
 					}
 				}
