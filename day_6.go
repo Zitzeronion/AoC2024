9a48277bfce3dbbbf9c73caef028a871834af41a
Ziteronion <physiknerd@gmail.com>
2025-09-04 09:23:02 +0200

    Solution star 1

--- day_6.go
+++ day_6.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	filename := "exampleinput_6.txt"
+	filename := "input_6.txt"
 	// fmt.Print(len(text))
 	// Loop over all lines in the file and print them.
 	content, err := os.ReadFile(filename)
@@ -15,16 +15,131 @@ func main() {
 		//Do something
 	}
 	lines := strings.Split(string(content), "\n")
-	fmt.Println(lines[0])
-	nchars := len(lines)
-	var text [nchars][n]string
-	for i := 0; i < len(lines); i++ {
+	n := len(lines) - 1
+	// fmt.Println(lines[0])
+
+	input := make([][]string, n)
+	path := make([][]int, n)
+
+	for i := 0; i < n; i++ {
 		newArr := strings.Split(lines[i], "") // split on each char
-		fmt.Println(newArr)
-		fmt.Println(len(newArr))
-		for j := 0; j < len(lines); j++ {
-			text[i][j] = newArr[j]
+
+		// fmt.Println(newArr)
+		// fmt.Println(len(newArr))
+		// fmt.Println(newArr[2])
+		for chars := 0; chars < len(newArr); chars++ {
+			input[i] = append(input[i], newArr[chars])
+			path[i] = append(path[i], 0)
+		}
+
+	}
+
+	// fmt.Println(input[1])
+	// fmt.Println(len(input[1]))
+	guard := getPos(input)
+	// fmt.Println(guard)
+	updateGuard := moveGuard(guard, input, path)
+	fmt.Println(updateGuard)
+
+	// fmt.Println(nUpdates, len(nUpdates))
+}
+
+func getPos(field [][]string) [2]int {
+	n := len(field[1])
+	pos := [2]int{0, 0}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if field[i][j] == "^" {
+				pos[0] = i
+				pos[1] = j
+				field[i][j] = "."
+			}
+		}
+
+	}
+	return pos
+}
+
+func moveGuard(guard [2]int, field [][]string, path [][]int) [2]int {
+	n := len(field[1])
+	for {
+		for i := guard[0] - 1; i > -1; i-- {
+			if field[i][guard[1]] == "." {
+				// Move horizontal
+				guard[0] = i
+				// fmt.Println(guard)
+				path[i][guard[1]] = 1
+			} else if field[i][guard[1]] == "#" {
+				// fmt.Println("Up dead end", guard) // up dead end
+				break
+			} else if i == 0 {
+				fmt.Println("Reached the border!")
+				break
+			}
+		}
+		if guard[0] == 0 || guard[0] == n-1 || guard[1] == 0 || guard[1] == n-1 {
+			break
+		}
+		for i := guard[1] + 1; i < n; i++ {
+			if field[guard[0]][i] == "." {
+				// Move to the right
+				guard[1] = i
+				// fmt.Println(guard)
+				path[guard[0]][i] = 1
+			} else if field[guard[0]][i] == "#" {
+				// fmt.Println("Right dead end", guard)
+				break
+			} else if guard[1] == n-1 {
+				fmt.Println("Reached the border!")
+				break
+			}
+		}
+		if guard[0] == 0 || guard[0] == n-1 || guard[1] == 0 || guard[1] == n-1 {
+			break
+		}
+		for i := guard[0] + 1; i < n; i++ {
+			if field[i][guard[1]] == "." {
+				// Move down
+				guard[0] = i
+				// fmt.Println(guard)
+				path[i][guard[1]] = 1
+			} else if field[i][guard[1]] == "#" {
+				// fmt.Println("Bottom dead end", guard)
+				break
+			} else if guard[0] == n-1 {
+				fmt.Println("Reached the border!")
+				break
+			}
+		}
+		if guard[0] == 0 || guard[0] == n-1 || guard[1] == 0 || guard[1] == n-1 {
+			break
+		}
+		for i := guard[1] - 1; i > -1; i-- {
+			if field[guard[0]][i] == "." {
+				// Move to the left
+				guard[1] = i
+				// fmt.Println(guard)
+				path[guard[0]][i] = 1
+			} else if field[guard[0]][i] == "#" {
+				// fmt.Println("Left dead end", guard)
+				break
+			} else if i == 0 {
+				fmt.Println("Reached the border!")
+				break
+			}
+		}
+		if guard[0] == 0 || guard[0] == n-1 || guard[1] == 0 || guard[1] == n-1 {
+			break
 		}
 	}
-	// fmt.Println(nUpdates, len(nUpdates))
+	sum := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			sum += path[i][j]
+		}
+	}
+	fmt.Println(sum)
+	// fmt.Println(path)
+	return guard
 }
