ca4d57cc5f2ba3efca8f0d702785c48569237121
Zitzeronion <zitzeronion@dynamic-pd01.res.v6.highway.a1.net>
2025-11-12 19:55:35 +0100

    Not so trivial actually

--- day_7.go
+++ day_7.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func main() {
@@ -16,13 +17,38 @@ func main() {
 	}
 	lines := strings.Split(string(content), "\n")
 	n := len(lines) - 1
-	fmt.Println(lines[0])
+	// fmt.Println(lines[0])
 
 	for i := 0; i < n; i++ {
 		leftside := strings.Split(lines[i], ":") // split on each char
-		fmt.Println("Result", leftside[0])
+		// fmt.Println("Result", leftside[0])
+		result, _ := strconv.Atoi(leftside[0])
+		fmt.Println(result)
 		rightside := strings.Fields(leftside[1])
-		fmt.Println("Terms", rightside)
+		var nums []int
+		// fmt.Println("Terms", rightside)
+		for j := 0; j < len(rightside); j++ {
+			element, _ := strconv.Atoi(rightside[j])
+			nums = append(nums, element)
+		}
+		sum := 0
+		prod := 1
+		for j := 0; j < len(rightside); j++ {
+			sum += nums[j]
+			prod *= nums[j]
+		}
+		if prod < result {
+			continue
+		}
+		calc2 := 0
+		for j := 0; j < len(rightside)-1; j++ {
+			if j%2 == 0 {
+				calc2 += nums[j] * nums[j+1]
+			}
+		}
+		fmt.Println(sum, prod, calc2)
+		fmt.Println(nums)
+
 		// fmt.Println(newArr[2])
 
 	}
